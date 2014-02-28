daf83629b0691385e40871a837b226acd5fcf81e
bjda <bjorndahlstrand@gmail.com>
2014-02-28 11:23:16 +0100

    init

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(hl_ruter.Startup))]
+
+namespace hl_ruter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
